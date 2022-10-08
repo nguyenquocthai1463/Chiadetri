34d45310f4c609217bea2abb7fbc756fba57d662
nguyenquocthai1463 <115343560+nguyenquocthai1463@users.noreply.github.com>
2022-10-08 15:27:23 +0700

    khoi tao git

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int tong(int a[],int l,int r){
+    int mid=(l+r)/2;
+    if(a[mid]%2==0){
+        return
+    }
+}
+
+int main() {
+    int base, exponent;
+    int sum;
+    cin >> base;
+    cin >> exponent;
+    sum = power(base,exponent);
+    cout<<sum;
+    return 0;
+}
